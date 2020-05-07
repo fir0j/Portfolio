30eb7adcb34e987c2bc4e832e0b4484cd068c156
fir0j <firoj.is.available@gmail.com>
2020-05-07 13:01:50 +0530

    added react-router-dom to the pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Fragment, useState } from 'react';
-import { LeftNavigation } from './components/LeftNavigation/LeftNavigation.component';
+import React, { Fragment } from 'react';
 import { RightNavigation } from './components/RightNavigation/RightNavigation.component';
 import { Logo } from './components/LeftNavigation/pages/Logo.page';
 import { Dashboard } from './components/LeftNavigation/pages/Dashboard.page';
@@ -8,50 +7,104 @@ import { Projects } from './components/LeftNavigation/pages/Projects.page';
 import { Blogs } from './components/LeftNavigation/pages/Blogs.page';
 import { ProgressTracker } from './components/LeftNavigation/pages/ProgressTracker.page';
 import { HireMe } from './components/LeftNavigation/pages/HireMe.page';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 const App = () => {
-	const [ activePage, setActivePage ] = useState('logo');
-
 	return (
 		<Fragment>
-			<Homepage activePage={activePage} setActivePage={setActivePage} />
+			<Homepage />
 		</Fragment>
 	);
 };
 
 export default App;
 
-const Homepage = ({ activePage, setActivePage }) => {
+const Homepage = () => {
+	const LeftNavigation = () => {
+		return (
+			<div className="flex flex-col h-full justify-between justify-content">
+				<Link to="/" className="h-full">
+					<div className="border h-full w-full flex justify-center items-center cursor-pointer bg-green-500">
+						Logo
+					</div>
+				</Link>
+
+				<Link to="/dashboard" className="h-full">
+					<div className="border h-full w-full flex justify-center items-center cursor-pointer bg-orange-500">
+						Dashboard
+					</div>
+				</Link>
+
+				<Link to="/resume" className="h-full">
+					<div className="border h-full w-full flex justify-center items-center cursor-pointer bg-blue-500">
+						Resume
+					</div>
+				</Link>
+
+				<Link to="/projects" className="h-full">
+					<div className="border h-full w-full flex justify-center items-center cursor-pointer bg-red-500">
+						Projects
+					</div>
+				</Link>
+
+				<Link to="/blogs" className="h-full">
+					<div className="border h-full w-full flex justify-center items-center cursor-pointer bg-yellow-500">
+						Blogs
+					</div>
+				</Link>
+
+				<Link to="/tracker" className="h-full">
+					<div className="border h-full w-full flex justify-center items-center cursor-pointer bg-indigo-500">
+						Progress Tracker
+					</div>
+				</Link>
+
+				<Link to="/hireme" className="h-full">
+					<div className="border h-full w-full flex justify-center items-center cursor-pointer bg-pink-500">
+						Hire Me
+					</div>
+				</Link>
+			</div>
+		);
+	};
+
 	return (
 		<div className="relative flex h-screen text-center border-green-500">
-			<div className="w-2/12 border">
-				<LeftNavigation setActivePage={setActivePage} />
-			</div>
-			<div className="w-full border">{activePage ? displayPage(activePage) : alert('No page to Display')}</div>
+			<Router>
+				<div className="w-2/12 border">
+					<LeftNavigation />
+				</div>
+
+				<div className="w-full border">
+					<Switch>
+						<Route exact path="/">
+							<Logo />
+						</Route>
+						<Route exact path="/dashboard">
+							<Dashboard />
+						</Route>
+						<Route exact path="/resume">
+							<Resume />
+						</Route>
+						<Route exact path="/projects">
+							<Projects />
+						</Route>
+						<Route exact path="/blogs">
+							<Blogs />
+						</Route>
+						<Route exact path="/tracker">
+							<ProgressTracker />
+						</Route>
+						<Route exact path="/hireme">
+							<HireMe />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+
 			<div className="h-screen border">
 				<RightNavigation />
 			</div>
 		</div>
 	);
-};
-
-const displayPage = (action) => {
-	switch (action) {
-		case 'logo':
-			return <Logo />;
-		case 'dashboard':
-			return <Dashboard />;
-		case 'resume':
-			return <Resume />;
-		case 'projects':
-			return <Projects />;
-		case 'blogs':
-			return <Blogs />;
-		case 'tracker':
-			return <ProgressTracker />;
-		case 'hireme':
-			return <HireMe />;
-		default:
-			return '';
-	}
 };
